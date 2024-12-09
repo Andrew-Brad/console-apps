d493a59f1f21db4a4ed8c32541bceb2ea70e799b
Andrew <me@andrewbrad.me>
2024-12-09 10:37:46 -0600

    add cli option sample

--- McMasterCliSample/Program.cs
+++ McMasterCliSample/Program.cs
@@ -11,12 +11,18 @@ public class Program
 
         // When a command executes, the raw string[] args value can be separated into two different categories: options and arguments.
         app.HelpOption();
-        var subject = app.Option("-s|--subject <SUBJECT>", "The subject", CommandOptionType.SingleValue);
-        subject.DefaultValue = "world";
+        var subjectOption = app.Option("-s|--subject <SUBJECT>", "The subject", CommandOptionType.SingleValue);
+        subjectOption.DefaultValue = "world";
+        var colorOption = app.Option("-c|--color <COLOR>", "Console color to print", CommandOptionType.SingleValue);
+        colorOption.DefaultValue = Console.ForegroundColor.ToString();
 
         app.OnExecute(() =>
         {
-            Console.WriteLine($"Hello {subject.Value()}!");
+            if (Enum.TryParse(colorOption.Value(), out ConsoleColor parsedColor))
+            {
+                Console.ForegroundColor = parsedColor;
+            }
+            Console.WriteLine($"Hello {subjectOption.Value()}!");
             return 0;
         });
 
