4df1e39eebfb7a05801a2a4a6aa8d703daacc557
Andrew <me@andrewbrad.me>
2024-12-09 10:17:53 -0600

    add console app for mcmaster cli utils

--- AsyncConsole/AsyncConsole/Program.cs
+++ AsyncConsole/AsyncConsole/Program.cs
@@ -6,13 +6,21 @@
 
         private static async Task MainAsync()
         {
-            Console.WriteLine("MainAsync()");
+            try
+            {
+                Console.WriteLine("MainAsync()");
 
-            Console.WriteLine("Delaying...");
-            await Task.Delay(500);//.ConfigureAwait(false);
-            Console.WriteLine("Delay Completed");
+                Console.WriteLine("Delaying...");
+                await Task.Delay(500);//.ConfigureAwait(false);
+                Console.WriteLine("Delay Completed");
 
-            Environment.Exit(0); // https://en.wikipedia.org/wiki/Exit_status
+                Environment.Exit(0); // https://en.wikipedia.org/wiki/Exit_status
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
         }
     }
 }
---
+++ AsyncConsole/FullCli/Program.cs
@@ -0,0 +1,24 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+namespace FullCli;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        // https://natemcmaster.github.io/CommandLineUtils/docs/intro.html
+        var app = new CommandLineApplication();
+
+        app.HelpOption();
+        var subject = app.Option("-s|--subject <SUBJECT>", "The subject", CommandOptionType.SingleValue);
+        subject.DefaultValue = "world";
+
+        app.OnExecute(() =>
+        {
+            Console.WriteLine($"Hello {subject.Value()}!");
+            return 0;
+        });
+
+        return app.Execute(args);
+    }
+}
