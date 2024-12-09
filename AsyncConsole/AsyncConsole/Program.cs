2746f6ddda1d5895e0d807b2cab576211eb203b9
Andrew <me@andrewbrad.me>
2024-12-09 10:00:14 -0600

    add async console project

---
+++ AsyncConsole/AsyncConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AsyncConsole
+{
+    public abstract class Program
+    {
+        public static Task Main() => MainAsync();
+
+        private static async Task MainAsync()
+        {
+            Console.WriteLine("MainAsync()");
+
+            Console.WriteLine("Delaying...");
+            await Task.Delay(500);//.ConfigureAwait(false);
+            Console.WriteLine("Delay Completed");
+
+            Environment.Exit(0); // https://en.wikipedia.org/wiki/Exit_status
+        }
+    }
+}
